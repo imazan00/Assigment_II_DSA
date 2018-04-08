d16793391258c90a8cf5c509f4a7fdccbdf3582f
imazan00 <i.mazan@innopolis.ru>
2018-04-08 19:58:41 +0300

    solve F

--- sorting_of_words.py
+++ sorting_of_words.py
@@ -1,20 +1,21 @@
 import string
 
 
-def counting_sort(arr, ind):
-    letter_count = dict.fromkeys(string.ascii_lowercase, 0)
-    letter_count[' '] = 0
+def sort_by_character(arr, ind, length):
+    possible_characters = ' ' + string.ascii_lowercase
+    letter_count = dict.fromkeys(possible_characters, [])
     for word in arr:
-        letter_count[word[ind]] += 1
-    return [(w + ' ') * letter_count[w[ind]] for w in arr if letter_count[w[ind]] > 0]
+        char = word[length - ind - 1]
+        letter_count[char] = letter_count[char] + [word]
+    return [w for ch in possible_characters for w in letter_count[ch]]
 
 
 def radix_sort(arr):
     max_len = len(max(arr, key=len))
     arr = [str(word) + (' ' * (max_len - len(word))) for word in arr]
-    for it in range(max_len):
-        counting_sort(arr, it)
-    return ''.join(word.replace(' ', '') + ' ' for word in arr)
+    for ind in range(max_len):
+        arr = sort_by_character(arr, ind, max_len)
+    return ''.join([word.replace(' ', '') + ' ' for word in arr])
 
 
 file = open("input.txt", "r")
@@ -23,5 +24,6 @@ array = [word for word in line.split(' ')]
 file.close()
 
 file = open("output.txt", "w")
+print(radix_sort(array))
 file.write(radix_sort(array))
 file.close()
